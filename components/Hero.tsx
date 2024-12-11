aabf296c7c37e90b7a7f3be4596b56236828f350
Chathushi Jayarathna <124165734+Chathu-Jayarathna@users.noreply.github.com>
2024-12-11 10:14:39 +0530

    &

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -57,7 +57,7 @@ const Hero = () => {
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Chathu, a Web Developer $ Designer
+            Hi! I&apos;m Chathu, a Web Developer & Designer
           </p>
 
 
